0c6d4d34538ad9707d05584857092b88ee497f93
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-08 23:15:04 -0300

    chore: add page files

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants/route_names.dart';
-import 'package:flutter_application_1/pages/home.dart';
+import 'package:flutter_application_1/pages/category_page.dart';
+import 'package:flutter_application_1/pages/home_page.dart';
+import 'package:flutter_application_1/pages/profile_page.dart';
+import 'package:flutter_application_1/pages/sign_in_page.dart';
+import 'package:flutter_application_1/pages/sign_up_page.dart';
 
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case RouteNames.signIn:
-      return MaterialPageRoute(builder: (_) => HomePage());
+      return MaterialPageRoute(builder: (_) => SignInPage());
 
     case RouteNames.signUp:
-      return MaterialPageRoute(builder: (_) => HomePage());
+      return MaterialPageRoute(builder: (_) => SignUpPage());
 
     case RouteNames.home:
       return MaterialPageRoute(builder: (_) => HomePage());
 
     case RouteNames.category:
-      return MaterialPageRoute(builder: (_) => HomePage());
+      return MaterialPageRoute(builder: (_) => CategoryPage());
 
     case RouteNames.profile:
-      return MaterialPageRoute(builder: (_) => HomePage());
+      return MaterialPageRoute(builder: (_) => ProfilePage());
 
     default:
       return MaterialPageRoute(
