59ce716b61cee02f7eba9177300c2f22b28f0b54
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-08 22:53:47 -0300

    chore: setup api service

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'http://localhost:8080';
+  
+  static const String users = '/users';
+  static const String categories = '/categories';
+  static const String tasks = '/tasks';
+} 
---
+++ lib/services/api_service.dart
@@ -0,0 +1,74 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  late final Dio _dio;
+  final String baseUrl;
+
+  ApiService({required this.baseUrl}) {
+    _dio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 3),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    ));
+
+    _dio.interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+    ));
+  }
+
+  Future<Response> get(String endpoint, {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(endpoint, queryParameters: queryParameters);
+      return response;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Response> post(String endpoint, {dynamic data}) async {
+    try {
+      final response = await _dio.post(endpoint, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Response> put(String endpoint, {dynamic data}) async {
+    try {
+      final response = await _dio.put(endpoint, data: data);
+      return response;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Future<Response> delete(String endpoint) async {
+    try {
+      final response = await _dio.delete(endpoint);
+      return response;
+    } on DioException catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  Exception _handleError(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+        return Exception('Erro de timeout na conexão');
+      case DioExceptionType.badResponse:
+        return Exception('Erro na resposta do servidor: ${error.response?.statusCode}');
+      case DioExceptionType.cancel:
+        return Exception('Requisição cancelada');
+      default:
+        return Exception('Erro na conexão: ${error.message}');
+    }
+  }
+} 
