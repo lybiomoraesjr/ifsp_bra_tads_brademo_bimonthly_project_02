39ef6b390951936dcace0a9cbf43e1bbef76f87b
ArthurDeFaria <arthur_de_faria@outlook.com>
2025-06-16 21:26:09 -0300

    feat: geolocation config and location_service

---
+++ lib/services/location_service.dart
@@ -0,0 +1,31 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  Future<Position> getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Serviço de localização desabilitado.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Permissão de localização negada.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      throw Exception('Permissão de localização permanentemente negada.');
+    }
+
+    return await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+      ),
+    );
+  }
+}
