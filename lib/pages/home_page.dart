2da7ba33440ff1f0f873062304e7837ea006cb54
ArthurDeFaria <arthur_de_faria@outlook.com>
2025-06-16 21:42:13 -0300

    fix: fix home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/services/location_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-        LocationService().getLocationByMap(context);
-          },
-          child: const Text('Abrir Mapa'),
-        ),
-      ),
+    return const Scaffold(
+      body: Center(child: Text('Home Page')),
     );
   }
 }
