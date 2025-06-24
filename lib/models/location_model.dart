6a841cba7e53f033841908f61d4c6b1507209b37
Lybio Moraes Junior <j.lybio@aluno.ifsp.edu.br>
2025-06-24 20:31:16 -0300

    feat: add Location model to represent API DTO

---
+++ lib/models/location_model.dart
@@ -0,0 +1,29 @@
+class Location {
+  final double latitude;
+  final double longitude;
+  final String? locationName;
+  final String? locationDescription;
+
+  Location({
+    required this.latitude,
+    required this.longitude,
+    this.locationName,
+    this.locationDescription,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) {
+    return Location(
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+      locationName: json['locationName'],
+      locationDescription: json['locationDescription'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'latitude': latitude,
+        'longitude': longitude,
+        'locationName': locationName,
+        'locationDescription': locationDescription,
+      };
+}
